f7d26a20abb6952a70a11486bee89244a05f5102
Sina Rahmannejad <46980823+sinarahmany@users.noreply.github.com>
2025-02-16 13:41:03 -0800

    added links and footer

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="relative isolate overflow-hidden bg-white min-h-screen p-6 flex flex-col items-center justify-center">
+  <div class="relative isolate overflow-hidden bg-white min-h-screen p-6 flex flex-col items-center justify-between">
+    
     <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
         <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff8080] to-[#4f39f6] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" />
       </div>
@@ -11,7 +12,7 @@
       </defs>
       <rect width="100%" height="100%" stroke-width="0" fill="url(#0787a7c5-978c-4f66-83c7-11c213f99cb7)" />
     </svg>
-    <div class="p-6 text-center">
+    <div class="p-6 text-center flex-grow flex items-center justify-center">
       
       <div class="">
     <!-- Title -->
@@ -210,10 +211,15 @@
       <p class="text-lg font-semibold text-gray-700">
         Processing... {{ processedCount }}/{{ images.length }}
       </p>
+    </div></div>
     </div>
-  </div>
     
-    </div>
+    <footer class="mt-16 border-t border-gray-900/10 pt-8 sm:mt-20 md:flex md:items-center md:justify-between lg:mt-24">
+        <div class="flex gap-x-6 md:order-2">
+          
+        </div>
+        <p class="mt-8 text-sm/6 text-gray-600 md:order-1 md:mt-0">&copy; 2025 Created by <a class="underline font-semibold" href="https://sinarahmannejad.com/" target="_blank">Sina Rahmannejad.</a></p>
+      </footer>
   </div>
   <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
   
